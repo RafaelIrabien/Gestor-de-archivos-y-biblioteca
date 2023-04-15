9be44de164d7fd13bf435a07125e90c7d278e0d5
RafaelIrabien <rafa.irabien@gmail.com>
2023-04-15 01:49:02 -0600

    Eliminar Carpeta de usuario con archivos y directorios{

--- procesos/usuario/eliminar/eliminarUsuario.php
+++ procesos/usuario/eliminar/eliminarUsuario.php
@@ -9,41 +9,40 @@
 	echo $usuarios->eliminarUsuario($id_Usuario);
 
 	//Definimos la ruta de la carpeta del usuario a eliminar
-	$carpeta2 = '../../../archivos/' . $id_Usuario; 
 	
+	//$carpetaCompartir = '../../../archivos/' . $id_Usuario . '/Archivos_Compartidos';
 
-	if (is_dir($carpeta2)) {
-        // Obtener todos los archivos y directorios dentro del directorio
+    function eliminarDirectorio($carpeta2) {
+    
+    if (is_dir($carpeta2)) {
         $archivos = scandir($carpeta2);
 
-        // Recorrer los archivos y directorios
         foreach ($archivos as $archivo) {
-            // Ignorar los directorios "." y ".."
-            if ($archivo != '.' && $archivo != '..') {
+            if ($archivo !== '.' && $archivo !== '..') {
                 $rutaArchivo = $carpeta2 . '/' . $archivo;
-                
-                // Verificar si el elemento es un archivo o un directorio
                 if (is_file($rutaArchivo)) {
-                    // Eliminar el archivo
-                    unlink($rutaArchivo);
-                } elseif (is_dir($rutaArchivo)) {
-                    // Llamar recursivamente a la función para eliminar el directorio
-                    mrdir($rutaArchivo);
+                    unlink($rutaArchivo); // Eliminar archivo
+                } else {
+                    eliminarDirectorio($rutaArchivo); // Eliminar directorio de forma recursiva
                 }
             }
         }
-
-
-        // Eliminar el directorio vacío
-        rmdir($carpeta2);
-        
-        return true;
-    } else {
-        return false;
+        rmdir($carpeta2); // Eliminar directorio vacío
     }
+}
+
+
+$carpeta2 = '../../../archivos/' . $id_Usuario; 
+eliminarDirectorio($carpeta2);
+
+
 	
-   			 
-    		     
+    
+
+
+    
+    
+    
 
 
 			 
